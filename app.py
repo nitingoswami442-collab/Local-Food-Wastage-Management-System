1858b45983f7e529ea4c2e735ca8195cea17380a
nitingoswami442-collab <nitingoswami442@gmail.com>
2026-06-22 00:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ st.title("🍲 Local Food Wastage Management System")
 
 menu = st.sidebar.selectbox(
     "Select Option",
-    ["Home", "Food Listings", "Provider Contacts"]
+    ["Home",
+     "Food Listings",
+     "Provider Contacts"
+    ]
 )
 
 # Home Page
@@ -21,6 +24,47 @@ if menu == "Home":
     This application helps reduce food wastage by connecting food providers with receivers.
     """)
 
+# Dashboard
+elif menu == "Dashboard":
+
+    st.header("📊 Dashboard")
+
+    total_providers = pd.read_sql_query(
+        "SELECT COUNT(*) AS cnt FROM providers", conn
+    ).iloc[0,0]
+
+    total_receivers = pd.read_sql_query(
+        "SELECT COUNT(*) AS cnt FROM receivers", conn
+    ).iloc[0,0]
+
+    total_food = pd.read_sql_query(
+        "SELECT COUNT(*) AS cnt FROM food_listings", conn
+    ).iloc[0,0]
+
+    total_claims = pd.read_sql_query(
+        "SELECT COUNT(*) AS cnt FROM claims", conn
+    ).iloc[0,0]
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Providers", total_providers)
+    col2.metric("Receivers", total_receivers)
+    col3.metric("Food Listings", total_food)
+    col4.metric("Claims", total_claims)
+
+    st.subheader("Claim Status")
+
+    claim_chart = pd.read_sql_query("""
+    SELECT Status,
+           COUNT(*) AS Total
+    FROM claims
+    GROUP BY Status
+    """, conn)
+
+    st.bar_chart(
+        claim_chart.set_index("Status")
+    )
+
 # Food Listings
 elif menu == "Food Listings":
     st.header("Available Food Listings")
@@ -29,7 +73,7 @@ elif menu == "Food Listings":
 
     st.dataframe(food)
 
-# Provider Contacts
+
 elif menu == "Provider Contacts":
     st.header("Provider Contacts")
 
